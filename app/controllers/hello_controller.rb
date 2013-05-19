ccc70d88f8897a57708f188d46281762e426e865
Kelsey Conophy <kelsecon+git@gmail.com>
2013-05-19 14:01:16 -0400

    rails app

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,10 @@
+class HelloController < ApplicationController
+  def world
+  	@name = "Bob"
+  end
+
+  def to
+  	# @name = params["name"] # a key, is a string, basically same as below
+  	@name = params[:name] # a key, is a symbol, is like a string, but it is stored internally differently within rails. It has memory implications & is faster to type
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = 'fcf12ff7442aeba914a001f04e737aa55bb2f14042b946aa5b97e0237d496dc298b90cff40475af60632f20e8f220b6f7e1bd4544c8e36270541603fd5f49481'
